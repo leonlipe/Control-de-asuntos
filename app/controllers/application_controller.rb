69ad866ccc30b3a8c54101dfbc0d590096518307
Leon Felipe Villavicencio Fernandez <leon@redleon.net>
2010-10-05 17:15:43 -0500

    Agregado el layout para mobile, por ahora iphone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  has_mobile_fu
   include AuthenticatedSystem
   include RoleRequirementSystem
 
@@ -10,24 +11,11 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-  before_filter :prepare_for_mobile
   before_filter :consulta_pendientes
   
 private
 
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
-
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
+ 
   
   def consulta_pendientes
      if (!current_user.nil?)
