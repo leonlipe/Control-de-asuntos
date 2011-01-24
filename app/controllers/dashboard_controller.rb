e10665e6f3f7607dfef65e02007759553f24e326
Leon Felipe Villavicencio Fernandez <leon@redleon.net>
2011-01-23 22:48:23 -0600

    Busqueda con ajax

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,10 +4,21 @@ class DashboardController < ApplicationController
   require_role :user
   
   def index
+    sort = case params['sort']
+               when "Fecha"  then "fecha"  
+               when "fecha_reverse"  then "fecha DESC"
+               end
+    asuntoparams = Asunto.create(params[:asunto])
+    conditions = ["fecha LIKE ? and nombresolicitante LIKE ? and organizacion LIKE ?", "%#{asuntoparams.fecha}%", "%#{asuntoparams.nombresolicitante}%","%#{asuntoparams.organizacion}%"]
+               
+   
     usuarios = Array.new(current_user.users)
     usuarios.push(current_user)
-    @asuntos = Asunto.paginate(:page => params[:page], :per_page => 5, :conditions => ["status_id <> ? and persona_turnado_id in (?)", configatron.status_terminado, usuarios ])
-
+    @asuntos = Asunto.paginate(:page => params[:page], :per_page => 5,:order => sort, :conditions => conditions)
+    #:conditions => ["status_id <> ? and persona_turnado_id in (?)", configatron.status_terminado, usuarios ])
+    if request.xml_http_request?
+         render :partial => "items_list", :layout => false
+       end
    
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -87,5 +87,27 @@ resultado
   		<td colspan='2' class='separator'></td>
   	</tr>"
   end
+  
+  def sort_td_class_helper(param)
+    result = 'class="sortup"' if params[:sort] == param
+    result = 'class="sortdown"' if params[:sort] == param + "_reverse"
+    return result
+  end
+
+     def sort_link_helper(text, param)
+       key = param
+       key += "_reverse" if params[:sort] == param
+       options = {
+           :url => {:action => 'list', :params => params.merge({:sort => key, :page => nil})},
+           :update => 'table',
+           :before => "$('#spinner').show()",
+           :success => "$('#spinner').hide()"
+       }
+       html_options = {
+         :title => "Sort by this field",
+         :href => url_for(:action => 'list', :params => params.merge({:sort => key, :page => nil}))
+       }
+       link_to_remote(text, options, html_options)
+     end
 
 end
