ee9b531fd8f94e2c8bd0744d98d40090a0b4e779
Leon Felipe Villavicencio Fernandez <leon@redleon.net>
2011-02-08 00:24:44 -0600

    Cambios en las busquedas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -88,6 +88,12 @@ resultado
   		<td colspan='2' class='separator'></td>
   	</tr>"
   end
+   
+  
+  def pertenece_a_mi_o_subordinados?(usuario, subordinados, usuario_propietario_id)
+  return true if subordinados.include?(usuario_propietario_id) or usuario.has_role?(:admin)
+  end
+ 
   
   def sort_td_class_helper(param)
     result = 'class="sortup"' if params[:sort] == param
@@ -99,23 +105,16 @@ resultado
        key = param
        key += "_reverse" if params[:sort] == param
        options = {
-           :url => {:action => 'list', :params => params.merge({:sort => key, :page => nil})},
+           :url => {:action => 'index', :params => params.merge({:sort => key, :page => params[:page], :asunto => params[:asunto]})},
            :update => 'table',
            :before => "$('#spinner').show()",
            :success => "$('#spinner').hide()"
        }
        html_options = {
          :title => "Sort by this field",
-         :href => url_for(:action => 'list', :params => params.merge({:sort => key, :page => nil}))
+         :href => url_for(:action => 'list', :params => params.merge({:sort => key, :page => params[:page], :asunto => params[:asunto]}))
        }
        link_to_remote(text, options, html_options)
      end
-     
- 
-  
-  def pertenece_a_mi_o_subordinados?(usuario, subordinados, usuario_propietario_id)
-  return true if subordinados.include?(usuario_propietario_id) or usuario.has_role?(:admin)
-  end
- 
 
 end
